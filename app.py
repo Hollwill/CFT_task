b88e9936c58a16277b6b01573492eaf1e7c79f8f
HollWill <22golomonzin@gmail.com>
2021-09-22 21:46:20 +0700

    docker problem fix

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from jinja2.exceptions import TemplateNotFound
 from flask.logging import default_handler
 from pythonjsonlogger import jsonlogger
 import logging
-from .utils import get_pil_image_from_base64, count_pixels_color_in_image
+import base64
+from io import BytesIO
+from PIL import ImageColor, Image
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -21,6 +23,22 @@ werkzeug_logger.handlers.append(default_handler)
 
 
 
+
+
+def count_pixels_color_in_image(image, hex_color: str):
+    if not hex_color.startswith('#'):
+        hex_color = '#' + hex_color
+    color_count = 0
+    rgb_color = ImageColor.getrgb(hex_color.strip())
+    for pixel in image.getdata():
+        if pixel[:3] == rgb_color:
+            color_count += 1
+    return color_count
+
+
+def get_pil_image_from_base64(img: str):
+    _, img_base64 = img.split(',')
+    return Image.open(BytesIO(base64.b64decode(img_base64)))
 
 
 @app.route('/api/black_or_white_more', methods=['POST'])
